8ab63ea7507dcd120206b0f16681d28f5396c934
Maximilian Vosswinkel <max.vosswinkel@gmx.de>
2025-05-19 13:04:13 +0200

    adding exception handling files

---
+++ src/main/java/storage/exception/EmailExistException.java
@@ -0,0 +1,10 @@
+package storage.exception;
+
+public class EmailExistException extends Exception {
+  
+	private static final long serialVersionUID = 1L;
+
+	public EmailExistException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/storage/exception/EmailNotFoundException.java
@@ -0,0 +1,10 @@
+package storage.exception;
+
+public class EmailNotFoundException extends Exception {
+  
+	private static final long serialVersionUID = 1L;
+
+	public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
